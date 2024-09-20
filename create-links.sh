d35b8520aa736ceeefa032fcda22f73238316dd6
Jeffrey Paine <jeff@jeffpaine.com>
2024-09-20 21:27:30 +0200

    Convert to simple inline commands

--- create-links.sh
+++ create-links.sh
@@ -5,24 +5,48 @@ set -u  # Exit if a referenced variable does not exist.
 set -o pipefail  # Don't hide errors in pipelines.
 set -x  # Print executed commands.
 
-[ -f ${HOME}/.bash_profile ] || ln -s ${PWD}/.bash_profile ${HOME}/.bash_profile
-[ -f ${HOME}/.bashrc ] || ln -s ${PWD}/.bashrc ${HOME}/.bashrc
-[ -f ${HOME}/.conkyrc ] || ln -s ${PWD}/.conkyrc ${HOME}/.conkyrc
-[ -f ${HOME}/.gitconfig ] || ln -s ${PWD}/.gitconfig ${HOME}/.gitconfig
-[ -f ${HOME}/.gitignore_global ] || ln -s ${PWD}/.gitignore_global ${HOME}/.gitignore_global
-[ -f ${HOME}/.inputrc ] || ln -s ${PWD}/.inputrc ${HOME}/.inputrc
-[ -f ${HOME}/.tmux.conf ] || ln -s ${PWD}/.tmux.conf ${HOME}/.tmux.conf
-[ -f ${HOME}/.vimrc ] || ln -s ${PWD}/.vimrc ${HOME}/.vimrc
+if [ ! -d "${HOME}/.dotfiles" ]; then
+    echo "ERROR: ${HOME}/.dotfiles must exist"
+    exit 1
+fi
+
+rm -f ${HOME}/.bash_profile
+rm -f ${HOME}/.bashrc
+rm -f ${HOME}/.conkyrc
+rm -f ${HOME}/.gitconfig
+rm -f ${HOME}/.gitignore_global
+rm -f ${HOME}/.inputrc
+rm -f ${HOME}/.tmux.conf
+rm -f ${HOME}/.vimrc
+
+ln -s ${HOME}/.dotfiles/.bash_profile ${HOME}/.bash_profile
+ln -s ${HOME}/.dotfiles/.bashrc ${HOME}/.bashrc
+ln -s ${HOME}/.dotfiles/.conkyrc ${HOME}/.conkyrc
+ln -s ${HOME}/.dotfiles/.gitconfig ${HOME}/.gitconfig
+ln -s ${HOME}/.dotfiles/.gitignore_global ${HOME}/.gitignore_global
+ln -s ${HOME}/.dotfiles/.inputrc ${HOME}/.inputrc
+ln -s ${HOME}/.dotfiles/.tmux.conf ${HOME}/.tmux.conf
+ln -s ${HOME}/.dotfiles/.vimrc ${HOME}/.vimrc
 
 # File paths that vary with OS.
 # https://www.gnu.org/software/bash/manual/html_node/Bash-Variables.html#index-OSTYPE.
+#
+# macOS.
 if [[ "${OSTYPE:?}" == "darwin"* ]]; then
-    # macOS.
-    [ -f ${HOME}/Library/Application\ Support/Code/User/settings.json ] || ln -s ${PWD}/vscode/settings.json ${HOME}/Library/Application\ Support/Code/User/settings.json
-    [ -f ${HOME}/Library/Application\ Support/Code/User/keybindings.json ] || ln -s ${PWD}/vscode/keybindings.json ${HOME}/Library/Application\ Support/Code/User/keybindings.json
+    rm -f "${HOME}/Library/Application\ Support/Code/User/settings.json"
+    rm -f "${HOME}/Library/Application\ Support/Code/User/keybindings.json"
+
+    mkdir -p "${HOME}/Library/Application\ Support/Code/User/"
+
+    ln -s "${HOME}/.dotfiles/vscode/settings.json" "${HOME}/Library/Application\ Support/Code/User/settings.json"
+    ln -s "${HOME}/.dotfiles/vscode/keybindings.json" "${HOME}/Library/Application\ Support/Code/User/keybindings.json"
+# Linux.
 else
-    # Assume Linux otherwise.
-    [ -d ${HOME}/.config/Code/User/ ] || mkdir -p ${HOME}/.config/Code/User
-    [ -f ${HOME}/.config/Code/User/settings.json ] || ln -s ${PWD}/vscode/settings.json ${HOME}/.config/Code/User/settings.json
-    [ -f ${HOME}/.config/Code/User/keybindings.json ] || ln -s ${PWD}/vscode/keybindings.json ${HOME}/.config/Code/User/keybindings.json
+    rm -f "${HOME}/.config/Code/User/settings.json"
+    rm -f "${HOME}/.config/Code/User/keybindings.json"
+
+    mkdir -p "${HOME}/.config/Code/User"
+
+    ln -s "${HOME}/.dotfiles/vscode/settings.json" "${HOME}/.config/Code/User/settings.json"
+    ln -s "${hOME}/.dotfiles/vscode/keybindings.json" "${HOME}/.config/Code/User/keybindings.json"
 fi
